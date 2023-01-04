0229a157bbc34eb6bdf7ea13963b7ac6721d8eba
Byron Ochieng <byronochieng01@gmail.com>
2023-01-05 02:48:16 +0300

    code corrections

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    output = ""
-    if i % 3 == 0:
-        output += "Fizz"
-    if i % 5 == 0:
-        output += "Buzz"
-    if output == "":
-        output = i
+def fizzbuzz():
+    for i in range(1, 101):
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        if output == "":
+            output = i
 
-    if i != 100:
-        print(output, end=" ")
-    else:
-        print(output)
+        if i != 100:
+            print(output, end=" ")
+        else:
+            print(output)
