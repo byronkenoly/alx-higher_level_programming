47f695ada5daa85a82c61087b92524f71b8d3914
Byron Ochieng <byronochieng01@gmail.com>
2023-01-05 02:32:22 +0300

    pycodestyle adherence

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 for i in range(1, 101):
-	output = ""
-	if i % 3 == 0:
-		output += "Fizz"
-	if i % 5 == 0:
-		output += "Buzz"
-	if output == "":
-		output = i
-	
-	if i != 100:
-		print(output, end = " ")
-	else:
-		print(output)
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if output == "":
+        output = i
+
+    if i != 100:
+        print(output, end=" ")
+    else:
+        print(output)
