ee45fd2c40c9a01328c7ca8dcaaf34abb2757d41
Byron Ochieng <byronochieng01@gmail.com>
2023-01-11 20:16:05 +0300

    removal of lambda expressions

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    square = lambda x: x * x
-
     x = 0
     y = 0
 
@@ -11,7 +9,7 @@ def square_matrix_simple(matrix=[]):
 
     for lst in copy:
         for i in lst:
-            new_val = square(i)
+            new_val = i ** 2
             copy[x][y] = new_val
             y += 1
         x += 1
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,10 +3,8 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
 
-    mul = lambda x: x * 2
-
     for key, val in new_dict.items():
-        change = {key: mul(val)}
+        change = {key: val * 2}
         new_dict.update(change)
 
     return new_dict
