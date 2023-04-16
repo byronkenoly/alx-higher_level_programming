bbf55449d4ed180e35a3f778a5a44f0ca9901824
Byron Ochieng <byronochieng01@gmail.com>
2023-04-16 23:29:03 +0300

    ensures code not executed when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,37 +3,37 @@
 import MySQLdb
 import sys
 
-"""
-Connecting to MySQL database
-"""
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    """
+    Connecting to MySQL database
+    """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
 
-"""
-Cursor object that allows one to interact with the
-MySQL database by executing queries and retrieving results
-"""
-cur = db.cursor()
+    """
+    Cursor object that allows one to interact with the
+    MySQL database by executing queries and retrieving results
+    """
+    cur = db.cursor()
 
-"""
-Executing MySQL queries
-"""
-all_states = cur.execute("SELECT * FROM states ORDER BY id ASC")
+    """
+    Executing MySQL queries
+    """
+    all_states = cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-"""
-Print the results
-"""
-results = cur.fetchall()
+    """
+    Print the results
+    """
+    results = cur.fetchall()
 
-for result in results:
-    print(result)
+    for result in results:
+        print(result)
 
-"""
-Close all cursors
-"""
-cur.close()
+    """
+    Close all cursors
+    """
+    cur.close()
 
-"""
-Close all databases
-"""
-db.close()
-
+    """
+    Close all databases
+    """
+    db.close()
