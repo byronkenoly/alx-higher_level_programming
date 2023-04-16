155129c7a1819f4fe3522178b9bf4353c3ac812b
Byron Ochieng <byronochieng01@gmail.com>
2023-04-16 23:21:51 +0300

    lists all cities from db

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connecting to MySQL database
+    """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
+
+    """
+    Cursor object that allows one to interact with the
+    MySQL database by executing queries and retrieving results
+    """
+    cur = db.cursor()
+
+    """
+    Executing MySQL queries
+    """
+    all_states = cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+
+    """
+    Print the results
+    """
+    results = cur.fetchall()
+
+    for result in results:
+        print(result)
+
+    """
+    Close all cursors
+    """
+    cur.close()
+
+    """
+    Close all databases
+    """
+    db.close()
