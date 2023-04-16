772ba59d05aaa5e9292b969f0991be65bf0fa7f4
Byron Ochieng <byronochieng01@gmail.com>
2023-04-16 23:20:58 +0300

    safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connecting to MySQL database
+    """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3])
+
+    """
+    Cursor object that allows one to interact with the
+    MySQL database by executing queries and retrieving results
+    """
+    cur = db.cursor()
+
+    """
+    Executing MySQL queries
+    """
+    all_states = cur.execute("SELECT * FROM states WHERE states.name LIKE %(name)s ORDER BY states.id ASC", {'name': sys.argv[4]})
+
+    """
+    Print the results
+    """
+    results = cur.fetchall()
+
+    for result in results:
+        print(result)
+
+    """
+    Close all cursors
+    """
+    cur.close()
+
+    """
+    Close all databases
+    """
+    db.close()
