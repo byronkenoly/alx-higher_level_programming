5a55c218a9aeca7abfa8ec60c371ceb4f3eccdad
Byron Ochieng <byronochieng01@gmail.com>
2023-06-15 20:50:28 +0300

    script that reads n prints contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that reads & prints content of a file
+
+const fs = require('fs').promises;
+
+async function readFile(file){
+    try {
+        const data = await fs.readFile(file, 'utf-8');
+	console.log(data.toString());
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+readFile(process.argv[2]);
